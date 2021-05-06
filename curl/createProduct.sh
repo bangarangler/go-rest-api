16f8b78f72a8afa93b7bac50518a9faae2664f1c
bangarangler <bangarangler@gmail.com>
2021-05-06 12:51:38 -0400

    adding some stuff

--- curl/createProduct.sh
+++ curl/createProduct.sh
@@ -2,6 +2,6 @@
 
 curl -X POST http://localhost:8010/product \
   -H "Content-Type: application/json" \
-  -d "{\"name\": \"to be deleted\", \"price\": 8.00}" \
+  -d "{\"name\": \"ryan\", \"price\": 1.00}" \
   -v | jq # need jq installed to pretty print json or remove the | jq
 
--- curl/updateProduct.sh
+++ curl/updateProduct.sh
@@ -2,6 +2,6 @@
 
 curl -X PUT http://localhost:8010/product/2 \
   -H "Content-Type: application/json" \
-  -d "{\"name\": \"updated product from curl\", \"price\": 70.00}" \
+  -d "{\"name\": \"updated epic product\", \"price\": 70.00}" \
   -v | jq # need jq installed to pretty print json or remove the | jq
 
